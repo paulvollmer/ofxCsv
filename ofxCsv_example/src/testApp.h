c9be271b6dc9de833cc377e89af4b2ba09c01835
Paul Vollmer <wrongentertainment@yahoo.de>
2012-03-23 14:51:04 +0100

    Changes at Xcode example

--- ofxCsv_example/src/testApp.h
+++ ofxCsv_example/src/testApp.h
@@ -1,28 +1,28 @@
 /**
- * wngCsv is developed by Paul Vollmer
+ * ofxCsv is developed by Paul Vollmer
  * http://www.wng.cc
  * 
  * 
- * Copyright (c) 2011 Paul Vollmer
+ * Copyright (c) 2011-2012 Paul Vollmer
  *
- * wngCsv is free software; you can redistribute it and/or
+ * ofxCsv is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  * 
- * wngCsv is distributed in the hope that it will be useful,
+ * ofxCsv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General
- * Public License along with wngCsv; if not, write to the
+ * Public License along with ofxCsv; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA  02111-1307  USA
  * 
  * @author      Paul Vollmer
- * @modified    2012.01.06
- * @version     0.1.0
+ * @modified    2012.03.23
+ * @version     0.1.2
  */
 
 
@@ -30,7 +30,7 @@
 #pragma once
 
 #include "ofMain.h"
-#include "wngCsv.h"
+#include "ofxCsv.h"
 
 using namespace wng;
 
@@ -55,6 +55,6 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 	
-		wngCsv csv;
+		ofxCsv csv;
 		
 };
